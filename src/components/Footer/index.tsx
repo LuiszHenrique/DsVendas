8d4811583d96025299f81ea2f799287934c325fe
Luis Henrique <luzim.henrique@gmail>
2021-11-11 15:28:38 -0400

    commit feito

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">App desenvolvido por <a href="https://github.com/LuiszHenrique" target="_blank" rel="noreferrer">Luis Henrique</a></p>
+        <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+          Evento promovido pela escola DevSuperior: <a href="https://instagram.com/LuzeRicky/" target="_blank" rel="noreferrer">@Luzericky</a></small></p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
